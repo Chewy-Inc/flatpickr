5ce5141fa1e0207660c07c4f80799f3d984645b2
Waldemar Figueroa <wfigueroa@chewy.com>
2019-03-15 16:52:56 -0400

    Add touch event for calendar close on outside click

--- src/index.ts
+++ src/index.ts
@@ -443,9 +443,12 @@ function FlatpickrInstance(
     if (!self.config.inline && !self.config.static)
       bind(window, "resize", debouncedResize);
 
-    if (window.ontouchstart !== undefined)
+    if (window.ontouchstart !== undefined) {
       bind(window.document, "click", documentClick);
-    else bind(window.document, "mousedown", onClick(documentClick));
+      bind(window.document, "touchstart", documentClick);
+    } else {
+      bind(window.document, "mousedown", onClick(documentClick));
+    }
     bind(window.document, "focus", documentClick, { capture: true });
 
     if (self.config.clickOpens === true) {
