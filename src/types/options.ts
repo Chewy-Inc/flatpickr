2ac7df8c0558229c0466e7415622c7874f9c9816
MansoorBashaBellary <mbellary@chewy.com>
2020-05-22 11:45:44 -0400

    fix: add focusable for next and previous icons

--- src/types/options.ts
+++ src/types/options.ts
@@ -348,7 +348,7 @@ export const defaults: ParsedOptions = {
   minuteIncrement: 5,
   mode: "single",
   nextArrow:
-    "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 17 17'><g></g><path d='M13.207 8.472l-7.854 7.854-0.707-0.707 7.146-7.146-7.146-7.148 0.707-0.707 7.854 7.854z' /></svg>",
+    "<svg focusable='false' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 17 17'><g></g><path d='M13.207 8.472l-7.854 7.854-0.707-0.707 7.146-7.146-7.146-7.148 0.707-0.707 7.854 7.854z' /></svg>",
   noCalendar: false,
   now: new Date(),
   onChange: [],
@@ -367,7 +367,7 @@ export const defaults: ParsedOptions = {
   position: "auto",
   positionElement: undefined,
   prevArrow:
-    "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 17 17'><g></g><path d='M5.207 8.471l7.146 7.147-0.707 0.707-7.853-7.854 7.854-7.853 0.707 0.707-7.147 7.146z' /></svg>",
+    "<svg focusable='false' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 17 17'><g></g><path d='M5.207 8.471l7.146 7.147-0.707 0.707-7.853-7.854 7.854-7.853 0.707 0.707-7.147 7.146z' /></svg>",
   shorthandCurrentMonth: false,
   showMonths: 1,
   static: false,
